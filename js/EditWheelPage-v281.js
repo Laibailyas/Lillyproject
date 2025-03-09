7f86ae0d1e515d9d38938a5adaa78d7e776595bd
Laiba Ilyas <laibailyas416@gmail.com>
2025-03-09 19:13:36 +0500

    Update EditWheelPage-v281.js

--- js/EditWheelPage-v281.js
+++ js/EditWheelPage-v281.js
@@ -5684,9 +5684,9 @@
           gt = (0, t.Uk)(
             "The Pop Wheel Random Question Generator is a versatile tool that offers endless possibilities when it comes to adding variety and excitement to any activity. Whether you're hosting a game night with friends or planning a team-building event at work, this tool can help break the ice and get people talking"
           ),
-          kt = (0, t._)("i", { class: "" }, null, -1),
+          kt = (0, t._)("i", { class: "fas fa-palette" }, null, -1),
           Wt = (0, t.Uk)(""),
-          yt = (0, t.Uk)(" at the top of the page. \n another text"),
+          yt = (0, t.Uk)(" at the top of the page." \n "another text"),
           _t = (0, t.Uk)(""),
           St = (0, t.Uk)(""),
           Ut = (0, t._)("br", null, null, -1),
